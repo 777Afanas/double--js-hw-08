50dd7296a9fbca7f860b3d188b0ab095767fa335
777Afanas <serhii2111@yahoo.com>
2023-06-23 19:04:04 +0300

    task 03 start

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
+
+
+const form = document.querySelector(".feedback-form");
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+form.addEventListener("input", saveMessage);
+
+function saveMessage(evt) {
+    evt.preventDefault();
+    const formElements = evt.currentTarget.elements;
+    const email = formElements.email.value;
+    const message = formElements.message.value;
+    const formData = { email, message, };
+
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
 
